02f2dbcfcbed94c352ea1e5629bfd87af3ba7453
lainbash <43439023+lainbash@users.noreply.github.com>
2021-01-18 13:07:26 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <?php //Login demonstration
-    require_once __DIR__ . "../module/Database/Database.php";
     require_once __DIR__ . "../module/Database/User.php";
     require_once __DIR__ . "../module/function.php";
     session_start();
